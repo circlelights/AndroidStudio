28895c27470df0aeabcf0d4477de2d1d1b927d95
Jason Isler <circlelights@me.com>
2014-12-30 16:01:10 -0500

    Version 2
    Added changes to the menu folder but the App is still not running
    properly.

--- app/src/main/java/com/circlelights/sunshine/ForecastFragment.java
+++ app/src/main/java/com/circlelights/sunshine/ForecastFragment.java
@@ -9,6 +9,9 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
@@ -29,10 +32,31 @@ public class ForecastFragment extends Fragment {
 
     public ForecastFragment() {
     }
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //Add this line for the fragment to handle menu events.
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater)  {
+        inflater.inflate(R.menu.forecastfragment, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)  {
+        int id = item.getItemId();
+        if (id == R.id.action_refresh) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
 
         ArrayList<String> Weather = new ArrayList<String>();
@@ -45,6 +69,8 @@ public class ForecastFragment extends Fragment {
 
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(),
                 R.layout.list_item_forecast, R.id.list_item_forecast_textview, Weather);
+
+
 
         ListView myListView = (ListView) rootView.findViewById(R.id.listView_forecast);
         myListView.setAdapter(adapter);
@@ -84,7 +110,8 @@ public class ForecastFragment extends Fragment {
                 StringBuffer buffer = new StringBuffer();
                 if (inputStream == null) {
                     // Nothing to do.
-                    forecastJsonStr = null;
+                   // forecastJsonStr = null;
+                    return null;
                 }
                 reader = new BufferedReader(new InputStreamReader(inputStream));
 
--- app/src/main/java/com/circlelights/sunshine/MainActivity.java
+++ app/src/main/java/com/circlelights/sunshine/MainActivity.java
@@ -11,8 +11,6 @@ public class MainActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-
         setContentView(R.layout.activity_main);
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
